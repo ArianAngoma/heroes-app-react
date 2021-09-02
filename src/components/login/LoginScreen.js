00622bb6b875f3fccb88959f83d52a760ee72b35
unknown <arian.angoma@tecsup.edu.pe>
2021-09-02 02:27:16 -0500

    History push/replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,22 @@
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+    const handleLogin = () => {
+        // Añade a la historia la ruta anterior
+        // history.push('/');
+
+        // Reemplaza la el historial que no visitó el login
+        history.replace('/');
+    }
+
     return (
-        <div>
-            <h1>Login Screen</h1>
+        <div className="container">
+            <h1>Login</h1>
+            <hr/>
+
+            <button
+                className="btn btn-primary"
+                onClick={handleLogin}>
+                Login
+            </button>
         </div>
     )
 }
