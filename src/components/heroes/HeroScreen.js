a70b7acf6df1e71ee5a94494b6fe408178ce2cad
unknown <arian.angoma@tecsup.edu.pe>
2021-09-03 23:07:35 -0500

    HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,17 +1,43 @@
 import {Redirect, useParams} from "react-router-dom";
 import {getHeroById} from "../../selectors/getHeroById";
+import {hydrate} from "react-dom";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
     const {heroId} = useParams();
     const hero = getHeroById(heroId);
 
     if (!hero) return <Redirect to="/"/>
 
+    const handleReturn = () => {
+        if (history.length <= 2) history.push('/');
+        else history.goBack();
+    }
+
     const {superhero, publisher, alter_ego, first_appearance, characters} = hero;
 
     return (
-        <div>
-            <h1>Heroes Screen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img src={`../assets/heroes/${heroId}.jpg`} alt={superhero}
+                     className="img-thumbnail"/>
+            </div>
+
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego: {alter_ego}</b></li>
+                    <li className="list-group-item"><b>Publisher: {publisher}</b></li>
+                    <li className="list-group-item"><b>First appearance: {first_appearance}</b></li>
+                </ul>
+
+                <h5>Characters</h5>
+                <p>{characters}</p>
+
+                <button className="btn btn-outline-info"
+                        onClick={handleReturn}>
+                    Return
+                </button>
+            </div>
         </div>
     )
 }
