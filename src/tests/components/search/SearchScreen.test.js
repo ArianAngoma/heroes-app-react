fa05538b106fc21a2459cb5ee8eccc2f12e30907
unknown <arian.angoma@tecsup.edu.pe>
2021-09-10 02:20:02 -0500

    Test on component SearchScreen

--- src/tests/components/search/SearchScreen.test.js
+++ src/tests/components/search/SearchScreen.test.js
@@ -24,4 +24,41 @@ describe('Pruebas en <SearchScreen/>', () => {
         expect(wrapper.find('input').prop('value')).toBe('batman');
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('Debe de mostrar un error si no se encuentra el hero', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batmanError']}>
+                <Route oath="/search" component={SearchScreen}/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.alert-danger').text().trim()).toBe('There is no a hero with batmanError')
+    });
+
+    test('Debe de llamar el push del history', () => {
+        const historyMock = {
+            push: jest.fn(),
+        };
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <Route oath="/search" component={() => <SearchScreen history={historyMock}/>}/>
+            </MemoryRouter>
+        );
+
+        wrapper.find('input').simulate('change', {
+            target: {
+                name: 'searchHero',
+                value: 'batman'
+            }
+        });
+
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault() {
+            }
+        });
+
+        expect(historyMock.push).toHaveBeenCalledWith('?q=batman');
+    });
 });
