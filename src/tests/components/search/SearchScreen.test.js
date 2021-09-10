5c292deb43bdcc8b234aace7091c92f688b41700
unknown <arian.angoma@tecsup.edu.pe>
2021-09-10 02:12:07 -0500

    Test on component SearchScreen

---
+++ src/tests/components/search/SearchScreen.test.js
@@ -0,0 +1,27 @@
+import {mount} from "enzyme";
+import {MemoryRouter, Route} from "react-router-dom";
+import {SearchScreen} from "../../../components/search/SearchScreen";
+
+describe('Pruebas en <SearchScreen/>', () => {
+    test('Debe de mostrarse correctamente con valores por defecto', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search']}>
+                <Route oath="/search" component={SearchScreen}/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.alert-info').text().trim()).toBe('Search a hero');
+    });
+
+    test('Debe de mostrar a Batman y el input con el valor del queryString', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <Route oath="/search" component={SearchScreen}/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('input').prop('value')).toBe('batman');
+        expect(wrapper).toMatchSnapshot();
+    });
+});
